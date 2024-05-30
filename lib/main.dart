d5a098e74737928c9051d9dcf66fdbc4134c6bdd
YashAPro1 <rekhasd01000001@gmail.com>
2024-05-30 11:16:14 +0530

    Started with user Interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,23 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GeoGemini App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -56,69 +46,170 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<CameraDescription> _cameras = [];
+  CameraController? _cameraController;
+  late FlutterSoundRecorder _recorder;
+  String? _audioFilePath;
+  bool _isRecording = false;
+  bool _isSendingAudio = false;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+    _recorder = FlutterSoundRecorder();
+    _recorder.openRecorder();
+  }
+
+  Future<void> initializeCamera() async {
+    _cameras = await availableCameras();
+    _cameraController = CameraController(_cameras[0], ResolutionPreset.high);
+    await _cameraController?.initialize();
+    setState(() {});
+  }
+
+  Future<void> startRecording() async {
+    if (await Permission.microphone.request().isGranted) {
+      final directory = await getApplicationDocumentsDirectory();
+      _audioFilePath =
+          '${directory.path}/${DateTime.now().millisecondsSinceEpoch}.aac';
+      await _recorder.startRecorder(toFile: _audioFilePath);
+      setState(() {
+        _isRecording = true;
+      });
+    }
+  }
+
+  Future<void> stopRecording() async {
+    await _recorder.stopRecorder();
+    setState(() {
+      _isRecording = false;
+      _isSendingAudio = true;
+    });
+  }
+
+  void sendAudio() {
+    // Implement sending audio logic here
+    setState(() {
+      _isSendingAudio = false;
+    });
+  }
+
+  Future<void> takePicture() async {
+    if (_cameraController != null) {
+      final image = await _cameraController!.takePicture();
+      // Implement sending picture logic here using image.path
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    _recorder.closeRecorder();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Color(0xFF222831),
+        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white, // Background color of the container
+                borderRadius: BorderRadius.circular(8.0), // Border radius
+              ),
+              child: IconButton(
+                icon: const ImageIcon(
+                  AssetImage(
+                      'assets/geogeminiicon.jpg'), // Use your custom image
+                  color: Colors.white,
+                ),
+                color: Colors.white,
+                onPressed: sendAudio,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('GeoGemini Chat Page', style: TextStyle(color: Colors.white)),
           ],
         ),
+        // title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      backgroundColor: Color(0xFF31363F),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                // Display messages here
+              ],
+            ),
+          ),
+          if (_isSendingAudio)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text('Audio recorded', style: TextStyle(color: Colors.white)),
+                  IconButton(
+                    icon: Icon(Icons.send),
+                    color: Colors.white,
+                    onPressed: sendAudio,
+                  ),
+                ],
+              ),
+            ),
+          Container(
+            decoration: const BoxDecoration(
+              // color: Colors.white,
+              border: Border(
+                top: BorderSide(color: Colors.grey, width: 0.5),
+                // bottom: BorderSide(color: Colors.grey, width: 1.0),
+              ),
+            ),
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.camera_alt),
+                  color: Colors.white,
+                  onPressed: takePicture,
+                ),
+                IconButton(
+                  icon: Icon(_isRecording ? Icons.stop : Icons.mic),
+                  color: Colors.white,
+                  onPressed: _isRecording ? stopRecording : startRecording,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Type a message',
+                    ),
+                    style: TextStyle(color: Colors.white),
+                    onSubmitted: (text) {
+                      // We will be sending message logic here
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  color: Colors.white,
+                  onPressed: () {
+                    // We will be sending message logic here
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
